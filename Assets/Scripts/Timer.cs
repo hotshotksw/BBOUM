f46735170b55ade1523583a84d2cf37432a66006
bethompson04 <benniert06@gmail.com>
2024-02-24 17:03:45 -0700

    GameManager and Timer implemented into mainScene
    the game manager and the time manager are now in the main scene.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private TMP_Text timerText;
+    enum TimerType {COUNTDOWN, STOPWATCH}
+    [SerializeField] private TimerType timerType;
+    [SerializeField] private float timeToDisplay = 60.0f;
+
+    private bool isRunning;
+
+    private void Awake()
+    {
+        timerText = GetComponent<TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.TimerStart += EventManagerOnTimerStart;
+        EventManager.TimerStop += EventManagerOnTimerStop;
+        EventManager.TimerUpdate += EventManagerOnTimerUpdate;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.TimerStart -= EventManagerOnTimerStart;
+        EventManager.TimerStop -= EventManagerOnTimerStop;
+        EventManager.TimerUpdate -= EventManagerOnTimerUpdate;
+    }
+
+    private void EventManagerOnTimerStart() => isRunning = true;
+    private void EventManagerOnTimerUpdate(float value) => timeToDisplay += value;
+    private void EventManagerOnTimerStop() => isRunning = false;
+
+    private void Update()
+    {
+        if (!isRunning) return;
+        if (timerType == TimerType.COUNTDOWN && timeToDisplay < 0.0f)
+        {
+            EventManager.OnTimerStop();
+            return;
+        }
+        timeToDisplay += timerType == TimerType.COUNTDOWN ? -Time.deltaTime : Time.deltaTime;
+
+        TimeSpan timeSpan = TimeSpan.FromSeconds(timeToDisplay);
+        timerText.text = timeSpan.ToString(format:@"mm\:ss\:ff");
+    }
+}
