eec23c4b8ed9f0a2e1922e63e538e837df554438
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-02-25 03:20:34 -0700

    Cutscene and Title Sound

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Cutscene : MonoBehaviour
 {
 	[SerializeField] Animator animator;
-
+	[SerializeField] private AudioSource music;
 	[SerializeField] VoidEvent endCutscene;
 
 	public void StartCutscene()
 	{
 		animator.SetTrigger("Start");
+		music.Play();
 	}
 
 	public void EndCutscene()
